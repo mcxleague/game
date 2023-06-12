a131c1b29ce2322adc2d06cd8db19f0a0fec57a8
mcxleague <119989048+mcxleague@users.noreply.github.com>
2023-06-12 11:33:56 -0700

    Update KeyPressListener.js

--- KeyPressListener.js
+++ KeyPressListener.js
@@ -1,27 +1,23 @@
 class KeyPressListener {
   constructor(keyCode, callback) {
-    let keySafe = true;
-    this.keydownFunction = function(event) {
-      if (event.code === keyCode) {
-         if (keySafe) {
-            keySafe = false;
-            callback();
-         }  
+    this.keySafe = true;
+    this.keydownFunction = (event) => {
+      if (event.code === keyCode && this.keySafe) {
+        this.keySafe = false;
+        callback();
       }
-   };
-   this.keyupFunction = function(event) {
+    };
+    this.keyupFunction = (event) => {
       if (event.code === keyCode) {
-         keySafe = true;
-      }         
-   };
-   document.addEventListener("keydown", this.keydownFunction);
-   document.addEventListener("keyup", this.keyupFunction);
+        this.keySafe = true;
+      }
+    };
+    document.addEventListener("keydown", this.keydownFunction);
+    document.addEventListener("keyup", this.keyupFunction);
   }
 
-  unbind() { 
+  unbind() {
     document.removeEventListener("keydown", this.keydownFunction);
     document.removeEventListener("keyup", this.keyupFunction);
   }
-
-
 }
